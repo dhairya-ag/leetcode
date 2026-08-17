4b5c72254969d4233fa8f46cc216550b7baf0641
dhairya-ag <dhairya.24scse1180172@galgotiasuniversity.ac.in>
2026-08-17 19:18:51 +0530

    Time: 12 ms (76.83%), Space: 78.8 MB (71.04%) - LeetHub

---
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minimumIndex(List<Integer> nums) {
+        int n = nums.size();
+        int candidate = 0;
+        int count = 0;
+        for(int num: nums){
+            if(count == 0){
+                candidate = num;
+            }
+            if(num == candidate){
+                count++;
+            } else{
+                count--;
+            }
+        }
+        int totalCount = 0;
+        for(int num: nums){
+            if(num == candidate){
+                totalCount++;
+            }
+        }
+        int leftCount = 0;
+        for(int i=0; i<n-1; i++){
+            if(nums.get(i)==candidate){
+                leftCount++;
+            }
+            int leftSize = i+1;
+            int rightSize = n - leftSize;
+            int rightCount = totalCount - leftCount;
+            if(leftCount > leftSize / 2 && rightCount > rightSize / 2) return i;
+        }
+        return -1;
+    }
+}
