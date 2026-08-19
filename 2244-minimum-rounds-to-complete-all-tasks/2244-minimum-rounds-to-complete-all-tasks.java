687207d33afdde6a6f59b93e079c1d0425fc84ec
dhairya-ag <dhairya.24scse1180172@galgotiasuniversity.ac.in>
2026-08-19 19:36:54 +0530

    Time: 30 ms (87.42%), Space: 108.4 MB (42.79%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumRounds(int[] tasks) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int task: tasks){
+            map.put(task, map.getOrDefault(task, 0)+1);
+        }
+        int count = 0;
+        for(int value: map.values()){
+            if(value == 1) return -1;
+            if(value == 2){
+                count += 1;
+            }else if(value == 3){
+                count += 1;
+            }else {
+                if(value % 3  == 0) count += value / 3;
+                else count += value / 3 + 1;
+            }
+        }
+        return count;
+    }
+}
