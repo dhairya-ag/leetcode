c91bb9f969b4f4acebcfaef53c43b081eba7338d
dhairya-ag <dhairya.24scse1180172@galgotiasuniversity.ac.in>
2026-08-19 18:53:22 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.71%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        return helper(n, dp);
+    }
+    private int helper(int n, int[] dp){
+        if(n <= 2) return n;
+        if(dp[n] != 0) return dp[n];
+        return dp[n] = helper(n-1, dp) + helper(n-2, dp);
+    }
+}
