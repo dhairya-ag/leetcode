e5e834e1e14c5bae3ff69df0c01efe9bdbce1e53
dhairya-ag <dhairya.24scse1180172@galgotiasuniversity.ac.in>
2026-08-19 19:20:47 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.26%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        int[] arr = new int[n+1];
+        Arrays.fill(arr,-1);
+        return helper(n,arr);
+
+    }
+    private int helper(int n,int[] dp){
+        if(n==0 || n==1) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = helper(n-1,dp) + helper(n-2,dp);
+    }
+}
