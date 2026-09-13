1068e78a6de68d61b316f6dff522019402df0057
dhairya-ag <dhairya.24scse1180172@galgotiasuniversity.ac.in>
2026-09-13 19:34:52 +0530

    Time: 233 ms (55.42%), Space: 42.6 MB (94.64%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    boolean solve(char[][] board) {
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] == '.') {
+                    for(char ch = '1'; ch <= '9'; ch++) {
+                        if(isSafe(board, i, j, ch)) {
+                            board[i][j] = ch;
+                            if(solve(board))
+                                return true;
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    boolean isSafe(char[][] board, int row, int col, char ch) {
+        for(int j = 0; j < 9; j++) {
+            if(board[row][j] == ch)
+                return false;
+        }
+        for(int i = 0; i < 9; i++) {
+            if(board[i][col] == ch)
+                return false;
+        }
+        int r = (row / 3) * 3;
+        int c = (col / 3) * 3;
+        for(int i = r; i < r + 3; i++) {
+            for(int j = c; j < c + 3; j++) {
+                if(board[i][j] == ch)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
