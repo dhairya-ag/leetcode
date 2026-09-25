d24994af9768bd96af7569b9efc13b2b97bd6819
dhairya-ag <dhairya.24scse1180172@galgotiasuniversity.ac.in>
2026-09-25 17:05:30 +0530

    Time: 3 ms (57.65%), Space: 46.5 MB (48.04%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
@@ -1,59 +1,16 @@
 class Solution {
     public List<String> removeAnagrams(String[] words) {
-
-        int size = words.length;
-
-        for (int i = 1; i < size; i++) {
-
-            if (words[i].length() != words[i - 1].length()) {
-                continue;
-            }
-
-            HashMap<Character, Integer> map = new HashMap<>();
-            for (char c : words[i].toCharArray()) {
-                map.put(c, map.getOrDefault(c, 0) + 1);
-            }
-
-            boolean anagram = true;
-            for (char c : words[i - 1].toCharArray()) {
-
-                if (!map.containsKey(c)) {
-                    anagram = false;
-                    break;
-                }
-
-                map.put(c, map.get(c) - 1);
-
-                if (map.get(c) < 0) {
-                    anagram = false;
-                    break;
-                }
-            }
-            if (anagram) {
-                for (int num : map.values()) {
-                    if (num != 0) {
-                        anagram = false;
-                        break;
-                    }
-                }
-            }
-
-            if (anagram) {
-                for (int index = i; index < size - 1; index++) {
-                    words[index] = words[index + 1];
-                }
-
-                size--;
-                i--;
+        List<String> ans = new ArrayList<>();
+        String prev = "";
+        for(String word : words){
+            char[] ch = word.toCharArray();
+            Arrays.sort(ch);
+            String s = new String(ch);
+            if(!s.equals(prev)){
+                ans.add(word);
+                prev = s;
             }
         }
-
-        List<String> result = new ArrayList<>();
-
-        for (int i = 0; i < size; i++) {
-            result.add(words[i]);
-        }
-
-        return result;
+        return ans;
     }
 }
